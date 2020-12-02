7f5533f64294fdabef7ecf76e93531234f903bc8
mixa3607 <30209772+mixa3607@users.noreply.github.com>
2020-12-02 17:47:58 +0500

    Init

---
+++ AuthKeyStealer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using Titanium.Web.Proxy;
+using Titanium.Web.Proxy.EventArguments;
+using Titanium.Web.Proxy.Models;
+
+namespace Genshin.KeyStealer
+{
+    public class AuthKeyStealer
+    {
+        private readonly string _genshinHost;
+        private readonly ProxyServer _proxyServer;
+        public AuthKeyStealer()
+        {
+            _proxyServer = new ProxyServer();
+            _genshinHost = "hk4e-api-os.mihoyo.com";
+        }
+
+        public void Start(string host, int port)
+        {
+            _proxyServer.BeforeRequest += OnBeforeRequest;
+            var proxySettings = new ExplicitProxyEndPoint(IPAddress.Parse(host), port);
+            _proxyServer.AddEndPoint(proxySettings);
+            _proxyServer.Start();
+            _proxyServer.SetAsSystemProxy(proxySettings, ProxyProtocolType.AllHttp);
+        }
+
+        public void Stop()
+        {
+            _proxyServer.RestoreOriginalProxySettings();
+            _proxyServer.BeforeRequest -= OnBeforeRequest;
+            _proxyServer.Stop();
+        }
+
+        private Task OnBeforeRequest(object sender, SessionEventArgs e)
+        {
+            var request = e.HttpClient.Request;
+            if (request.Host == _genshinHost)
+            {
+                var qPaths = request.RequestUri.Query.Split('?', '&');
+                foreach (var qPath in qPaths)
+                {
+                    if (qPath.StartsWith("authkey", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Console.WriteLine(HttpUtility.UrlDecode(qPath.Split('=')[1]));
+                        Console.WriteLine("Press any key");
+                    }
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+namespace Genshin.KeyStealer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stealer = new AuthKeyStealer();
+            stealer.Start("127.0.0.1", 3500);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+            stealer.Stop();
+        }
+    }
+}
